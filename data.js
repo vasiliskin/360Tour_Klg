29784418bdb35687b7081e71c0ac27cb70ce9856
VASILY RYABCHIKOV <mr.vasiliskin@hotmail.com>
2023-12-12 13:34:56 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01_panorama_3",
+      "name": "01_Panorama_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1469,
+      "initialViewParameters": {
+        "yaw": 0.1792527587071433,
+        "pitch": 0.23018416802450758,
+        "fov": 1.4956932554011342
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
